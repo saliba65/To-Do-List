faa32019cbb315d046887e0a1ae1db4241df9ee2
Lucas Saliba <lucassaliba10@gmail.com>
2020-10-31 14:16:12 -0300

    Hacktober

--- src/Todo.js
+++ src/Todo.js
@@ -2,12 +2,14 @@ import React from "react";
 
 export default function Todo({ todo, toggleTodo }) {
   function handleTodoClick() {
+    console.log("handleToDo");
     toggleTodo(todo.id);
   }
 
   return (
     <div>
       <label>
+        <p>oi</p>
         <input
           type="checkbox"
           checked={todo.complete}
--- src/TodoList.js
+++ src/TodoList.js
@@ -3,6 +3,7 @@ import Todo from "./Todo";
 
 export default function TodoList({ todos, toggleTodo }) {
   return todos.map((todo) => {
+    console.log("Funcao ToDo ");
     return <Todo key={todo.id} toggleTodo={toggleTodo} todo={todo} />;
   });
 }
